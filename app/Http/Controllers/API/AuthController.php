d5082f57b65287b9d815f0d7f3151dadd0cdad35
Gabriel Ibenye <gabrielibenye@gmail.com>
2022-06-10 22:00:09 +0100

    registering user

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,9 +3,46 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Traits\ApiResponder;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    use ApiResponder;
+
+    public function register(Request $request)
+    {
+        // validate the request
+        $request->validate([
+            'phone_number' => ['required', 'string', 'max:15', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+
+        // dd($request->all());
+        // create the user
+        $user = User::create([
+            'phone_number' => $request->phone_number,
+            'password' => Hash::make($request->password),
+        ]);
+
+
+        //create token for user
+        $token = $user->createToken("access Token")->plainTextToken;
+
+        //TODO: Create OTP
+        //TODO: Send OTP to user phone
+
+        // return the token
+        return $this->successResponse(
+            "Registeration Successful",
+            [
+                "user" => new $user,
+                "token" => $token,
+            ],
+            Response::HTTP_CREATED
+        );
+    }
 }
