9aebe71719489276ed0e249fba8fbf9b6e958e28
kazeems <seidukazeemolawale@gmail.com>
2022-06-10 11:51:14 +0100

    UserDetail model, factory, migration created

---
+++ app/Models/UserDetail.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserDetail extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'first_name',
+        'last_name',
+        'other_names',
+        'date_of_birth',
+        'gender',
+        'referral_code',
+        'referrer'
+    ];
+}
---
+++ database/migrations/2022_06_10_103017_create_user_details_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_details', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('other_names')->nullable();
+            $table->string('date_of_birth');
+            $table->enum('gender',['male','female']);
+            $table->string('referral_code')->nullable();
+            $table->string('referrer')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_details');
+    }
+};
