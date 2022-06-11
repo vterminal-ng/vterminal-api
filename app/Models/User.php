b60aeb8ba7eae8eb163cd99b7bce0991b8ba208e
Gabriel Ibenye <gabrielibenye@gmail.com>
2022-06-11 17:58:28 +0100

    user relationshit with otp model

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,4 +43,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function otp()
+    {
+        return $this->hasOne(Otp::class);
+    }
 }
