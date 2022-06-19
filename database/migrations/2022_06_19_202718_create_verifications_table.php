9a5b59a35ab03e7df41c6d6fec9e950f57929788
kazeems <seidukazeemolawale@gmail.com>
2022-06-19 21:38:03 +0100

    Verifications model, factory, migration created

---
+++ database/migrations/2022_06_19_202718_create_verifications_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('verifications', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->enum('identity_type',['bvn','voters_card','nin','drivers_license']);
+            $table->unsignedBigInteger('identity_number')->unique();
+            $table->string('id');
+            $table->string('passport');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('middle_name');
+            $table->string('date_of_birth');
+            $table->string('reference');
+            $table->string('payload');
+            $table->string('phone_number')->unique();
+            $table->enum('gender',['male','female']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('verifications');
+    }
+};
