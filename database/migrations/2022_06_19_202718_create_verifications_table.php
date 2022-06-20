c5e9d736c866dde863bdb88b5d02beaf06a0141a
Gabriel Ibenye <gabrielibenye@gmail.com>
2022-06-20 09:06:10 +0100

    commented error in migration

--- database/migrations/2022_06_19_202718_create_verifications_table.php
+++ database/migrations/2022_06_19_202718_create_verifications_table.php
@@ -16,9 +16,9 @@ return new class extends Migration
         Schema::create('verifications', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->enum('identity_type',['bvn','voters_card','nin','drivers_license']);
+            $table->enum('identity_type', ['bvn', 'voters_card', 'nin', 'drivers_license']);
             $table->unsignedBigInteger('identity_number')->unique();
-            $table->string('id');
+            // $table->string('id');
             $table->string('passport');
             $table->string('first_name');
             $table->string('last_name');
@@ -27,7 +27,7 @@ return new class extends Migration
             $table->string('reference');
             $table->string('payload');
             $table->string('phone_number')->unique();
-            $table->enum('gender',['male','female']);
+            $table->enum('gender', ['male', 'female']);
             $table->timestamps();
         });
     }
