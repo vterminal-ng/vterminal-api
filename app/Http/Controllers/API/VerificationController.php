bc5edb75334fa63676b03b35181ad339b7576545
kazeems <seidukazeemolawale@gmail.com>
2022-07-12 11:55:42 +0100

    Revert changes in verification controller

--- app/Http/Controllers/API/VerificationController.php
+++ app/Http/Controllers/API/VerificationController.php
@@ -3,11 +3,8 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use App\Models\Verification;
-use App\Services\FlutterwaveService;
 use App\Traits\ApiResponder;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
 class VerificationController extends Controller
 {
