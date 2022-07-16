82461a0735305e3cd74e2e48721cd141d05e02c6
Gabriel Ibenye <gabrielibenye@gmail.com>
2022-07-16 03:10:42 +0100

    alternative function for sending http requests

--- app/Traits/ConsumeExternalService.php
+++ app/Traits/ConsumeExternalService.php
@@ -3,10 +3,20 @@
 namespace App\Traits;
 
 use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Http;
 
 trait ConsumeExternalService
 {
 
+    /**
+     * Send Http requests and throws exception for all error responses
+     *
+     * @param  mixed $method
+     * @param  mixed $endpoint
+     * @param  mixed $formParams
+     * @param  mixed $headers
+     * @return JsonString
+     */
     public function performRequest($method, $endpoint, $formParams = [], $headers = [])
     {
         $client = new Client([
@@ -17,8 +27,35 @@ trait ConsumeExternalService
             $headers['Authorization'] = "Bearer $this->secret";
         }
 
-        $reponse = $client->request($method, $endpoint, ['form_params' => $formParams, 'headers' => $headers]);
+        $response = $client->request($method, $endpoint, ['form_params' => $formParams, 'headers' => $headers]);
 
-        return $reponse->getBody()->getContents();
+        return $response->getBody()->getContents();
+    }
+
+    /**
+     * Sends Http requests and does not throw an exception for error responses except 500 level errors
+     * 
+     * @param  mixed $method
+     * @param  mixed $endpoint
+     * @param  mixed $formParams
+     * @param  mixed $headers
+     * @return JsonString
+     */
+    public function makeRequest($method, $endpoint, $formParams = [], $headers = [])
+    {
+        if (isset($this->secret)) {
+            $headers['Authorization'] = "Bearer $this->secret";
+        }
+
+        $response = Http::withHeaders($headers)
+            ->{strtolower($method)}(
+                $this->baseUri . $endpoint,
+                $formParams
+            );
+
+        // throw an exception for error level 500
+        $response->throwIf($response->serverError());
+
+        return $response->body();
     }
 }
