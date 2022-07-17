1b7ae20c9bfaf5ea809623e5783b4e0578451168
Gabriel Ibenye <gabrielibenye@gmail.com>
2022-07-17 02:58:22 +0100

    withdrwal funtion

--- app/Http/Controllers/API/WalletController.php
+++ app/Http/Controllers/API/WalletController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Services\PaystackService;
+use App\Traits\ApiResponder;
 use App\Traits\Generators;
 use GrahamCampbell\ResultType\Success;
 use Illuminate\Http\Request;
 
 class WalletController extends Controller
 {
+    use ApiResponder;
+
+    protected $paystackService;
+
+    public function __construct(PaystackService $paystackService)
+    {
+        $this->paystackService = $paystackService;
+    }
 
     public function withdraw(Request $request)
     {
@@ -18,16 +29,23 @@ class WalletController extends Controller
         ]);
 
         // get user object of auth user
+        $user = User::find(auth()->id());
 
         // initialize transfer paystack request
+        $response = $this->paystackService->initiateTransfer($request->amount, $user->bankDetail->recipient_code);
 
         // get the transfer code from above request
+        $transferCode = $response->data->transfer_code;
 
         // finalize transfer paystack request
+        // TODO: Disable OTP in the paystack portal
+        $response = $this->paystackService->finalizeTransfer($transferCode);
 
         // if transation was successful, withdraw from user wallet,
+        $user->withdraw($request->amount);
 
         // return Success
+        return $this->successResponse("Withdrawal Complete");
     }
 
     public function deposit(Request $request)
