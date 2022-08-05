6862de5422a8a5ebcec6c1a0cf93798559c0b420
Gabriel Ibenye <gabrielibenye@gmail.com>
2022-08-05 06:13:31 +0100

    added relationship in pin migration

--- database/migrations/2022_08_03_204846_create_pins_table.php
+++ database/migrations/2022_08_03_204846_create_pins_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('pin');
             $table->enum('status', ['active', 'inactive']);
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
