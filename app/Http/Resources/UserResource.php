402e292e0b6cf0b168050feb6300fb660ff279f3
Gabriel Ibenye <gabrielibenye@gmail.com>
2022-09-16 05:50:20 +0100

    rollback

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -26,7 +26,7 @@ class UserResource extends JsonResource
             "userDetails" => new UserDetailResource($this->userDetail),
             "bankDetails" => new BankDetailResource($this->bankDetail),
             "walletBalance" => $this->balance,
-            "walletTransactions" => WalletTransactionResource::collection($this->transactions),
+            "walletTransactions" => $this->transactions,
             "card" => new AuthorizedCardResource($this->authorizedCard),
         ];
 
