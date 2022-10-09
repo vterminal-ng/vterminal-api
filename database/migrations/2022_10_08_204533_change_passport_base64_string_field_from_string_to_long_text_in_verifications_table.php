6e4ee4b99c149ff1ae244b9039050a9f70b105df
Gabriel Ibenye <gabrielibenye@gmail.com>
2022-10-09 01:47:54 +0100

    changed column type

---
+++ database/migrations/2022_10_08_204533_change_passport_base64_string_field_from_string_to_long_text_in_verifications_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('verifications', function (Blueprint $table) {
+            $table->longText('passport_base64_string')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('string_to_long_text_in_verifications', function (Blueprint $table) {
+            //
+        });
+    }
+};
