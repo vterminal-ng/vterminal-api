38b6ef14435677ad3645c76370e70bc244ef7c1f
Jeremiah Ekundayo <dayoolapeju@outlook.com>
2022-10-12 07:04:41 +0100

    Update getFullName attribute consumption

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -64,7 +64,7 @@
                                             <img src="../../assets/media/image/user/man_avatar3.jpg"
                                                  class="rounded-circle" alt="avatar">
                                         </figure>
-                                        {{ $user->userDetail->getFullNameAttribute() }}
+                                        {{ $user->userDetail->full_name }}
                                     </a>
                                 </td>
                                 <td>{{ $user->email }}</td>
