0abe6b9f31eeec80b6e61a7755171a2899047d1c
Gabriel Ibenye <gabrielibenye@gmail.com>
2022-10-18 14:30:11 +0100

    test

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -12,7 +12,7 @@ use App\Models\Code;
 class DashboardController extends Controller
 {
     public function displayDashboard()
-    {   
+    {
         // Users
         $usersCount = User::count();
         $customersCount = User::where('role', 'customer')->count();
@@ -27,45 +27,45 @@ class DashboardController extends Controller
 
         // Total amount of code transactions
         $totalWalletBalance = 0;
-        foreach(User::all() as $user){
+        foreach (User::all() as $user) {
             $totalWalletBalance += $user->balance;
         }
 
         // Total Amount in wallets (Customer)
         $customerWalletsBalance = 0;
-        foreach(User::where('role', 'customer')->get() as $user){
+        foreach (User::where('role', 'customer')->get() as $user) {
             $customerWalletsBalance += $user->balance;
         }
 
         // Total Amount in wallets (Merchant)
         $merchantWalletsBalance = 0;
-        foreach(User::where('role', 'merchant')->get() as $user){
+        foreach (User::where('role', 'merchant')->get() as $user) {
             $merchantWalletsBalance += $user->balance;
         }
 
         return view(
-            'admin.dashboard', 
+            'admin.dashboard',
             compact('usersCount', 'customersCount', 'merchantsCount', 'codesCount', 'activeCodesCount', 'pendingCodesCount', 'cancelledCodesCount', 'completedCodesCount', 'totalWalletBalance', 'customerWalletsBalance', 'merchantWalletsBalance')
         );
     }
 
     public function getUsers(Request $request)
-    {   
+    {
         // All Users
         $users = User::with('userDetail', 'merchantDetail')->latest()->get();
 
-        if($request->has('status')){
+        if ($request->has('status')) {
             $status = $request->query('status');
 
-            if($status === 'active')
+            if ($status === 'active')
                 // Get active users
                 $users = User::where('updated_at', '>=', Carbon::today()->subMonth())->with('userDetail')->latest()->get();
 
-            if($status === 'inactive')
+            if ($status === 'inactive')
                 // Get inactive users
                 $users = User::whereBetween('updated_at', [Carbon::today()->subMonths(3), Carbon::today()->subMonth()])->with('userDetail')->latest()->get();
 
-            if($status === 'dormant')
+            if ($status === 'dormant')
                 // Get dormant users
                 $users = User::where('updated_at', '<', Carbon::today()->subMonths(3))->with('userDetail')->latest()->get();
         }
@@ -74,36 +74,37 @@ class DashboardController extends Controller
     }
 
     public function getUserDetails(User $user)
-    {   
+    {
         $user->load('userDetail', 'merchantDetail', 'customerCodes.customer', 'merchantCodes.merchant');
         return view('admin.user_details', compact('user'));
     }
 
     public function changeUserStatus(User $user)
-    {   
+    {
         $status = $user->is_active;
-		$user->update(['is_active' => !$status]);
+        $user->update(['is_active' => !$status]);
 
         return back()->with('success', 'User status updated succesfully!');
     }
 
     public function getCustomers(Request $request)
-    {   
+    {
         // All Customers
-        $users = User::where('role', 'customer')->with('userDetail')->latest()->get();
+        // $users = User::where('role', 'customer')->with('userDetail')->latest()->get();
+        $users = User::where('role', 'merchant')->with('userDetail')->latest()->get();
 
-        if($request->has('status')){
+        if ($request->has('status')) {
             $status = $request->query('status');
 
-            if($status === 'active')
+            if ($status === 'active')
                 // Get active customers
                 $users = User::where('role', 'customer')->where('updated_at', '>=', Carbon::today()->subMonth())->with('userDetail')->latest()->get();
 
-            if($status === 'inactive')
+            if ($status === 'inactive')
                 // Get inactive customers
                 $users = User::where('role', 'customer')->whereBetween('updated_at', [Carbon::today()->subMonths(3), Carbon::today()->subMonth()])->with('userDetail')->latest()->get();
 
-            if($status === 'dormant')
+            if ($status === 'dormant')
                 // Get dormant customers
                 $users = User::where('role', 'customer')->where('updated_at', '<', Carbon::today()->subMonths(3))->with('userDetail')->latest()->get();
         }
@@ -112,22 +113,22 @@ class DashboardController extends Controller
     }
 
     public function getMerchants(Request $request)
-    {   
+    {
         // All Merchants
         $users = User::where('role', 'merchant')->with('userDetail')->latest()->get();
 
-        if($request->has('status')){
+        if ($request->has('status')) {
             $status = $request->query('status');
 
-            if($status === 'active')
+            if ($status === 'active')
                 // Get active merchants
                 $users = User::where('role', 'merchant')->where('updated_at', '>=', Carbon::today()->subMonth())->with('userDetail')->latest()->get();
 
-            if($status === 'inactive')
+            if ($status === 'inactive')
                 // Get inactive merchants
                 $users = User::where('role', 'merchant')->whereBetween('updated_at', [Carbon::today()->subMonths(3), Carbon::today()->subMonth()])->with('userDetail')->latest()->get();
 
-            if($status === 'dormant')
+            if ($status === 'dormant')
                 // Get dormant merchants
                 $users = User::where('role', 'merchant')->where('updated_at', '<', Carbon::today()->subMonths(3))->with('userDetail')->latest()->get();
         }
