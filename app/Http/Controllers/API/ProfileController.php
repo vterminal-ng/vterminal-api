ff4c2abd9f35aac40be988d7b38160a7be501bf5
Jeremiah Ekundayo <dayoolapeju@outlook.com>
2022-11-20 12:05:08 +0100

    API key controller functions implemetation

--- app/Http/Controllers/API/ProfileController.php
+++ app/Http/Controllers/API/ProfileController.php
@@ -68,21 +68,47 @@ class ProfileController extends Controller
 
     public function generateApiKey(Request $request)
     {
-        //
+        // Generate an API KEY
+        $api_key = "pk_" . floor(microtime(true) * 1000);
+
+        $user = auth()->user();
+
+        $user->api_key = $api_key;
+        $user->save();
+
+        return $this->successResponse(
+            "API KEY Generated",
+            [
+                "api_key" => $api_key
+            ]
+        );
     }
 
     public function reGenerateApiKey(Request $request)
     {
-        //
+        $user = auth()->user();
+
+        $api_key = "pk_" . floor(microtime(true) * 1000);
+
+        $user->api_key = $api_key;
+        $user->save();
+
+        return $this->successResponse(
+            "API KEY Updated",
+            [
+                "api_key" => $api_key
+            ]
+        );
     }
 
     public function getApiKey()
     {
-        //
-        $api_key  = User::where('id', auth()->id())->pluck('api_key');
+        $api_key = auth()->user()->api_key;
+
+        if(!$api_key) return $this->failueResponse("User hasn't generated an API KEY", Response::HTTP_NO_CONTENT);
 
         return $this->successResponse(
-            "User's API KEY",
+            "API KEY",
             [
                 "api_key" => $api_key
             ]
