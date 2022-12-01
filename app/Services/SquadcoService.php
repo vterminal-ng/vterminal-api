88b1af2b0f5bf5a857c8aa4b6cadc767a6520541
Gabriel Ibenye <gabrielibenye@gmail.com>
2022-12-01 21:36:57 +0100

    squadco virtual account endpoints added

--- app/Services/SquadcoService.php
+++ app/Services/SquadcoService.php
@@ -68,6 +68,7 @@ class SquadcoService
     public function refundTransaction($gateway_ref, $reference, $refundType = "Full", $refundAmount = null, $reason = "Refunding first payment on card made to save card")
     {
         //refund type can be "Full" or "Partial"
+        // refund_amount is only required when refund_type is Partial
         $response = $this->makeRequest(
             'POST',
             "/transaction/refund",
@@ -77,6 +78,61 @@ class SquadcoService
                 "reason_for_refund" => $reason,
                 "transaction_ref" => $reference,
                 "refund_amount" => $refundAmount
+            ]
+        );
+        // dd($response);
+
+        return json_decode((string)$response);
+    }
+
+    /**
+     * CreateVirtualAccountsForCustomer
+     *
+     * @param  mixed $firstName
+     * @param  mixed $lastName
+     * @param  mixed $dob
+     * @param  mixed $mobile
+     * @param  mixed $bvn
+     * @param  mixed $gender
+     * @param  mixed $address
+     * @param  mixed $customerId
+     * @param  mixed $middleName (optional)
+     * @param  mixed $email (optional)
+     * @return void
+     */
+    public function CreateVirtualAccountsForCustomer($firstName, $lastName,  $dob, $mobile, $bvn, $gender, $address, $customerId, $middleName = null, $email = null)
+    {
+        $response = $this->performRequest(
+            'POST',
+            "/virtual-account",
+            [
+                "first_name" => $firstName,
+                "last_name" => $lastName,
+                "middle_name" => $middleName,
+                "dob" => $dob,
+                "mobile_num" => $mobile,
+                "email" => $email,
+                "bvn" => $bvn,
+                "gender" => $gender,
+                "address" => $address,
+                "customer_identifier" => $customerId
+            ]
+        );
+        // dd($response);
+
+        return json_decode((string)$response);
+    }
+
+    public function CreateVirtualAccountsForBusiness($businessName, $mobile, $bvn, $customerId)
+    {
+        $response = $this->performRequest(
+            'POST',
+            "/virtual-account",
+            [
+                "business_name" => $businessName,
+                "mobile_num" => $mobile,
+                "bvn" => $bvn,
+                "customer_identifier" => $customerId
             ]
         );
         // dd($response);
