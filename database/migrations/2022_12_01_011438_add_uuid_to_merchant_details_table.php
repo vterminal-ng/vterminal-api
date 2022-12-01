cb79e55d1c3e537c84cc6d24ead2f254edb0f60a
Gabriel Ibenye <gabrielibenye@gmail.com>
2022-12-01 21:37:23 +0100

    added uuid to merchant and user details table

---
+++ database/migrations/2022_12_01_011438_add_uuid_to_merchant_details_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('merchant_details', function (Blueprint $table) {
+            $table->uuid()->default(Str::uuid());
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('merchant_details', function (Blueprint $table) {
+            $table->dropColumn('uuid');
+        });
+    }
+};
