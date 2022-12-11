c5438dd17db4d94ce0b686969ab94397da66fc33
Gabriel Ibenye <gabrielibenye@gmail.com>
2022-12-11 16:30:48 +0100

    added recurring

--- app/Services/SquadcoService.php
+++ app/Services/SquadcoService.php
@@ -34,6 +34,7 @@ class SquadcoService
                 "transaction_ref" => $reference,
                 "payment_channels" => ["card"],
                 "pass_charge" => true,
+                "is_recurring" => $transactionType == TransactionType::ADD_CARD ? true : false,
                 "metadata" => $metadata,
             ]
         );
