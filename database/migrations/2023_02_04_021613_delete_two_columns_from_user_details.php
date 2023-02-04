10f566b2d6ff10feb7359c67f39ff6a8d0b8e502
Gabriel Ibenye <gabrielibenye@gmail.com>
2023-02-04 02:19:37 +0100

    deleted redundant colums from user details

---
+++ database/migrations/2023_02_04_021613_delete_two_columns_from_user_details.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_details', function (Blueprint $table) {
+            $table->dropColumn('dedicated_account_no');
+            $table->dropColumn('dedicated_bank_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_details', function (Blueprint $table) {
+            $table->string('dedicated_account_no')->nullable();
+            $table->string('dedicated_bank_name')->nullable();
+        });
+    }
+};
